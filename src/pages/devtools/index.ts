c0bf3064ec47f9e1752071c341f071c19447ec7e
JCHU634 <87676426+jchu634@users.noreply.github.com>
2025-07-16 18:09:14 +1200

    Fixed paths for devtools panel icon

--- src/pages/devtools/index.ts
+++ src/pages/devtools/index.ts
@@ -1,23 +1,29 @@
 import Browser from "webextension-polyfill";
 
 const CHROMIUM_PANEL_DIR = "src/pages/devtools/panel.html";
+const CHROMIUM_ICON_DIR = "icon-32.png";
 const FIREFOX_PANEL_DIR = "panel.html";
+const FIREFOX_ICON_DIR = "../../../icon-32.png";
 
 let finalPanelContentPath: string;
+let finalIconPath: string;
 
 if (navigator.userAgent.includes("Firefox")) {
     finalPanelContentPath = FIREFOX_PANEL_DIR;
+    finalIconPath = FIREFOX_ICON_DIR;
 } else if (
     navigator.userAgent.includes("Chrome") ||
     navigator.userAgent.includes("Edge")
 ) {
     finalPanelContentPath = CHROMIUM_PANEL_DIR;
+    finalIconPath = CHROMIUM_ICON_DIR;
 } else {
     // Fallback for unknown browsers
     console.warn(
         "Unknown browser, defaulting to Chrome-style path for DevTools panel."
     );
     finalPanelContentPath = CHROMIUM_PANEL_DIR;
+    finalIconPath = CHROMIUM_ICON_DIR;
 }
 
 console.log(
@@ -25,7 +31,7 @@ console.log(
 );
 
 Browser.devtools.panels
-    .create("OPFS Browser", "public/icon-32.png", finalPanelContentPath)
+    .create("OPFS Browser", finalIconPath, finalPanelContentPath)
     .catch((error) => {
         console.error("[DevTools Loader] Error creating panel:", error);
     });
